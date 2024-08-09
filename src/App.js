312ae8bce784ef9682870662514c9911cea3b060
ukrWily <ukrwily@gmail.com>
2024-08-09 13:48:51 +0300

    L &&

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
   const incrementCount = () => {
     setCount(count + 1);
   };
-
+  const resetCount = () => {
+    setCount(0);
+  };
   //
   return (
     <div className="App">
@@ -17,6 +19,17 @@ function App() {
       <Button onClick={incrementCount} />
       <Button onClick={incrementCount} />
       <Button onClick={incrementCount} />
+      {/* if count > 0 -> show button */}
+      {count > 0 && (
+        <div>
+          <button
+            style={{ backgroundColor: "lightgreen" }}
+            onClick={resetCount}
+          >
+            Reset
+          </button>
+        </div>
+      )}
     </div>
   );
 }
